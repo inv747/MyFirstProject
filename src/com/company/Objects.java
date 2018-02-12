90ebe20274f3cd3635756109bfc0717b7ecb8453
inv747 <inwestor767@gmail.com>
2018-02-12 20:54:33 +0100

    Add text to my label

--- src/com/company/Objects.java
+++ src/com/company/Objects.java
@@ -5,12 +5,17 @@ import javax.swing.*;
 public class Objects {
     public static void main(String[] args) {
         //create our window
-        JFrame window = new JFrame();
-        window.setTitle("A piece of shit");
+        JFrame window = new JFrame(); // window variable that holds JFrame objects, and we're creating a new JFrame to assign to our variable
+        window.setTitle("A piece of shit"); // we're calling methods on our JFrame object to configure it
         window.setSize(800, 600);
         window.setVisible(true);
 
-        JLabel label = new JLabel();
-        label.setText("My label text");
+        JLabel label = new JLabel(); // label variable holds JLabel objects, and we're creating a new JLabel to assign to our variable
+        label.setText("My label text"); // we're calling the setText method on our JLabel objects to set its text property
+
+        window.add(label); // we're calling the add m() of our JFrame object to add our label to the window variable
+
+        String s = "hi";
+
     }
 }
