5460dc067ca04e8b1163338d1b48cf311ba860b8
inv747 <inwestor767@gmail.com>
2018-02-12 20:24:05 +0100

    Initial commit

---
+++ src/com/company/Objects.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Objects {
+    public static void main(String[] args) {
+        //create our window
+        JFrame window = new JFrame();
+        window.setTitle("A piece of shit");
+        window.setSize(800, 600);
+        window.setVisible(true);
+        
+    }
+}
